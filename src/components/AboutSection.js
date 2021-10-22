eef05e1854e2f32b487772eb50936a56dcc9f27c
Shahin <shahin.zeynalov95@gmail.com>
2021-10-22 22:03:26 +0400

    test

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,30 +1,48 @@
 import React from "react";
 import home1 from "../img/home1.png";
-
+import styled from "styled-components";
 
 const AboutSection = () => {
-  return(
-      <div>
-          <div className="description">
-              <div className="title">
-                  <div className="hide">
-                      <h2>We work to make</h2>
-                  </div>
-                  <div className="hide">
-                      <h2>your <span>dreams</span> come  </h2>
-                  </div>
-                  <div className="hide">
-                      <h2>ture.</h2>
-                  </div>
-              </div>
-              <p>Contact us for any Photography ideas that you have.We have professionals with amazing skills</p>
-              <button>Contact Us</button>
-          </div>
-          <div className="image">
-              <img src={home1} alt="gwc"/>
-          </div>
-      </div>
-  )
+    return (
+        <About>
+            <Description>
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>your <span>dreams</span> come </h2>
+                    </div>
+                    <div className="hide">
+                        <h2>ture.</h2>
+                    </div>
+                </div>
+                <p>Contact us for any Photography ideas that you have.We have professionals with amazing skills</p>
+                <button>Contact Us</button>
+            </Description>
+            <div className="image">
+                <img src={home1} alt="gwc"/>
+            </div>
+        </About>
+    )
 }
+
+//styled-component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`
+
 
 export default AboutSection;
---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return(
+    <div className='faq'>
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className="question">
+            <h4>How do i start ?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, iste?</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>How do i start ?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, iste?</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>How do i start ?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, iste?</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>How do i start ?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, iste?</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FaqSection;
